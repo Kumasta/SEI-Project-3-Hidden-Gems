43e34a780275e5038e917de5e903ffe7a6e3a839
Mayur Kumar <mayurtheraj@googlemail.com>
2022-02-09 16:41:06 +0000

    Added a seed pin

--- back-end/seeds/data/pins.js
+++ back-end/seeds/data/pins.js
@@ -8,6 +8,16 @@ export default [
     tags: ['Test'], //Array of strings
     latitude: 1, //Number
     longitude: 1, //Number
+  },
+  {
+    title: 'Waterlook bridge - Great view of London Thames sites', //String
+    typeOfPlace: 'vista', //String
+    description: 'Great place to see Big ben, london Eye, the shard and other Iconic buildings in one place.', //String
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/e/e4/River_Thames_and_Waterloo_Bridge%2C_London-17Aug2009.jpg', //String
+    status: true, //Boolean
+    tags: [ 'city walk', 'city skyline' ], //Array of strings
+    latitude: 51.50852877586289, //Number
+    longitude: -0.11696577072143556, //Number
   }
 ] 
 
