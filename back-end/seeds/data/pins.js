0fd6e4232f223c4f61d3bbe28ab80d6250e1aa63
Mayur Kumar <mayurtheraj@googlemail.com>
2022-02-09 17:13:41 +0000

    Test 2

--- back-end/seeds/data/pins.js
+++ back-end/seeds/data/pins.js
@@ -2,7 +2,7 @@ export default [
   {
     title: 'Kaws', //String
     typeOfPlace: 'Art Exhibition', //String
-    description: 'In his first major solo exhibition in London, NEW FICTION, KAWS (Brian Donnelly b.1974) presents new and recent works in physical and augmented reality at Serpentine North from 18 January – 27 February 2022. The exhibition is presented in collaboration with Acute Art and curated by Acute Art’s Artistic Director, Daniel Birnbaum. A parallel digital version of the show launches simultaneously in Fortnite, a video game developed by Epic Games.', //String
+    description: 'In his first major solo exhibition in London, NEW FICTION, KAWS (Brian Donnelly b.1974) presents new and recent works in physical and augmented reality at Serpentine North from 18 January - 27 February 2022. The exhibition is presented in collaboration with Acute Art and curated by Acute Art\'s Artistic Director, Daniel Birnbaum. A parallel digital version of the show launches simultaneously in Fortnite, a video game developed by Epic Games.', //String
     imageUrl: 'https://d37zoqglehb9o7.cloudfront.net/uploads/2022/01/%C2%A9Jonty-Wilde_KAWS_NEWFICTION-20220080-1335x1001.jpg ', //String
     status: true, //Boolean
     tags: [], //Array of strings
@@ -12,7 +12,7 @@ export default [
   {
     title: 'ArcelorMittal Orbit', //String
     typeOfPlace: 'Landmark with a twist', //String
-    description: 'Discover the world’s tallest and longest tunnel slide. Travel up to 15 mph down the UK’s tallest public artwork through light and dark sections as London’s dramatic skyscape passes by. Enjoy breathtaking views of London from the observation platform.', //String
+    description: 'Discover the world\'s tallest and longest tunnel slide. Travel up to 15 mph down the UK\'s tallest public artwork through light and dark sections as London\'s dramatic skyscape passes by. Enjoy breathtaking views of London from the observation platform.', //String
     imageUrl: 'https://d37zoqglehb9o7.cloudfront.net/uploads/2022/01/%C2%A9Jonty-Wilde_KAWS_NEWFICTION-20220080-1335x1001.jpg ', //String
     status: true, //Boolean
     tags: [], //Array of strings
@@ -28,15 +28,15 @@ export default [
     tags: [], //Array of strings
     latitude: 51.5227734, //Number
     longitude: -0.10626521794643069, //Number
-   },
-   {
+  },
+  {
     title: 'Jet Star Camping',
     typeOfPlace: 'Hotel',
     description: 'Originally carrying up to nine passengers, the aircraft was built in the 1970s and still has its original features intact, such as a lit bar area, cockpit with Xbox and a first-class ship cabin for a double bedroom.',
     imageUrl: 'https://www.farawaylucy.com/wp-content/uploads/2017/05/DSC_2063.jpg',
     status: true,
     tags: ['hotel'],
-    latitude: -4.7772600797718106, 
+    latitude: -4.7772600797718106,
     longitude: 51.705811496716116,
   },
   {
@@ -55,7 +55,7 @@ export default [
     description: 'Great place to see Big ben, london Eye, the shard and other Iconic buildings in one place.', //String
     imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/e/e4/River_Thames_and_Waterloo_Bridge%2C_London-17Aug2009.jpg', //String
     status: true, //Boolean
-    tags: [ 'city walk', 'city skyline' ], //Array of strings
+    tags: ['city walk', 'city skyline'], //Array of strings
     latitude: 51.50852877586289, //Number
     longitude: -0.11696577072143556, //Number
   },
@@ -69,5 +69,5 @@ export default [
     latitude: 51.4676965, //Number
     longitude: -0.0234944, //Number
   }
-] 
+]
 
