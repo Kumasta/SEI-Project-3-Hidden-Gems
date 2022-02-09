c83d60b442d47f92afd7c6e4152c7eb07ca46509
Tom Murphy <thomasjosephmurphy@hotmail.com>
2022-02-09 16:39:50 +0000

    Tom Seeds

--- back-end/seeds/data/pins.js
+++ back-end/seeds/data/pins.js
@@ -1,13 +1,13 @@
 export default [
   {
-    title: 'Test', //String
-    typeOfPlace: 'Test', //String
-    description: 'TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest', //String
-    imageUrl: 'Test', //String
-    status: true, //Boolean
-    tags: ['Test'], //Array of strings
-    latitude: 1, //Number
-    longitude: 1, //Number
+    title: 'Jet Star Camping',
+    typeOfPlace: 'Hotel',
+    description: 'Originally carrying up to nine passengers, the aircraft was built in the 1970s and still has its original features intact, such as a lit bar area, cockpit with Xbox and a first-class ship cabin for a double bedroom.',
+    imageUrl: 'https://www.farawaylucy.com/wp-content/uploads/2017/05/DSC_2063.jpg',
+    status: true,
+    tags: ['hotel'],
+    latitude: -4.7772600797718106, 
+    longitude: 51.705811496716116,
   }
 ] 
 
