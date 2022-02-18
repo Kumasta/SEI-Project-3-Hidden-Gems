4f9950f43c907a70139b617724cede81367f386a
Tom Murphy <thomasjosephmurphy@hotmail.com>
2022-02-18 11:47:24 +0000

    styling and seeds

--- back-end/seeds/data/users.js
+++ back-end/seeds/data/users.js
@@ -46,5 +46,41 @@ export default [
       bio: '',
       profilePicURL: '',
     },
+  },
+  {
+    username: 'Benedict Cumberbatch',
+    email: 'benedictus@mail.com',
+    password: 'benedictus',
+    passwordConfirmation: 'benedictus',
+    superUser: false,
+    profile: {
+      name: 'Benedict Cumberbatch',
+      bio: 'I have been a dragon, a genius detective and a wizard. What have you done with your life?',
+      profilePicURL: 'https://assets.vogue.com/photos/5ab5674150ba394ce3a683f5/1:1/w_354%2Cc_limit/00-promo-benedict-cumberbatch.jpg',
+    },
+  },
+  {
+    username: 'Benedict-Cumberbatch',
+    email: 'benedictus@mail.com',
+    password: 'benedictus',
+    passwordConfirmation: 'benedictus',
+    superUser: false,
+    profile: {
+      name: 'Benedict Cumberbatch',
+      bio: 'I have been a dragon, a genius detective and a wizard. What have you done with your life?',
+      profilePicURL: 'https://assets.vogue.com/photos/5ab5674150ba394ce3a683f5/1:1/w_354%2Cc_limit/00-promo-benedict-cumberbatch.jpg',
+    },
+  },
+  {
+    username: 'Egglover',
+    email: 'Egglover@mail.com',
+    password: 'Egglover',
+    passwordConfirmation: 'Egglover',
+    superUser: false,
+    profile: {
+      name: 'Egglover',
+      bio: 'I love egg.',
+      profilePicURL: 'https://media.istockphoto.com/photos/plain-brown-egg-standing-on-white-surface-picture-id177444800?k=20&m=177444800&s=612x612&w=0&h=Wyp0mf5XnhXjk0tQGVGSfs0_0pBXtsjmPhu-crgJVow=',
+    },
   }
 ]
