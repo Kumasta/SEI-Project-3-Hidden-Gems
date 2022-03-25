0dcb541c5aed5cdde9ce03a400b69e36e86fff3a
Mayur Kumar <mayurtheraj@googlemail.com>
2022-03-25 11:00:56 +0000

    Some odd processs with git

--- front-end/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './styles/main.scss'
-import 'mapbox-gl/dist/mapbox-gl.css'
-import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
